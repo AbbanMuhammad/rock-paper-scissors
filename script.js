59fedbfed04e4b77bf68603cffd3a1fd01bffd30
Sani Ismail <saniismail2@gmail.com>
2025-07-12 11:40:37 +0100

    function named getComputerChoice was created with code to randomly return one of the string valualues : 'rock', 'paper' or 'scissors'.

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+const { use } = require("react");
+
 console.log("Hello, World!");
+function getComputerChoice() {
+  const randomChoice = Math.random();
+  if (randomChoice < 1 / 3) {
+    return "rock";
+  } else if (randomChoice < 2 / 3) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
+}
+
+console.log(getComputerChoice());
